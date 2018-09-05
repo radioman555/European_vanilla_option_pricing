f8668ec83067168e50c9ff3988dc9241c27d2e6d
Aubrey McKinnon <a.mckinnon96@hotmail.co.uk>
2018-09-05 14:49:29 +0100

    European vanilla pricing option programme - monte carlo

--- main.cpp
+++ main.cpp
@@ -1,49 +1,63 @@
-#define _USE_MATH_DEFINES
-
-#include <iostream>
+#include <algorithm>    // Needed for the "max" function
 #include <cmath>
+#include <iostream>
+
 using namespace std;
 
-// Standard normal probability density function
-double norm_pdf(const double& x) {
-    return (1.0/(pow(2*M_PI,0.5)))*exp(-0.5*x*x);
+// A simple implementation of the Box-Muller algorithm, used to generate
+// gaussian random numbers - necessary for the Monte Carlo method below
+// Note that C++11 actually provides std::normal_distribution<> in
+// the <random> library, which can be used instead of this function
+double gaussian_box_muller() {
+    double x = 0.0;
+    double y = 0.0;
+    double euclid_sq = 0.0;
+
+    // Continue generating two uniform random variables
+    // until the square of their "euclidean distance"
+    // is less than unity
+    do {
+        x = 2.0 * rand() / static_cast<double>(RAND_MAX)-1;
+        y = 2.0 * rand() / static_cast<double>(RAND_MAX)-1;
+        euclid_sq = x*x + y*y;
+    } while (euclid_sq >= 1.0);
+
+    return x*sqrt(-2*log(euclid_sq)/euclid_sq);
 }
 
-// An approximation to the cumulative distribution function
-// for the standard normal distribution
-// Note: This is a recursive function
-double norm_cdf(const double& x) {
-    double k = 1.0/(1.0 + 0.2316419*x);
-    double k_sum = k*(0.319381530 + k*(-0.356563782 + k*(1.781477937 + k*(-1.821255978 + 1.330274429*k))));
+// Pricing a European vanilla call option with a Monte Carlo method
+double monte_carlo_call_price(const int& num_sims, const double& S, const double& K, const double& r, const double& v, const double& T) {
+    double S_adjust = S * exp(T*(r-0.5*v*v));
+    double S_cur = 0.0;
+    double payoff_sum = 0.0;
 
-    if (x >= 0.0) {
-        return (1.0 - (1.0/(pow(2*M_PI,0.5)))*exp(-0.5*x*x) * k_sum);
-    } else {
-        return 1.0 - norm_cdf(-x);
+    for (int i=0; i<num_sims; i++) {
+        double gauss_bm = gaussian_box_muller();
+        S_cur = S_adjust * exp(sqrt(v*v*T)*gauss_bm);
+        payoff_sum += std::max(S_cur - K, 0.0);
     }
+
+    return (payoff_sum / static_cast<double>(num_sims)) * exp(-r*T);
 }
 
-// This calculates d_j, for j in {1,2}. This term appears in the closed
-// form solution for the European call or put price
-double d_j(const int& j, const double& S, const double& K, const double& r, const double& v, const double& T) {
-    return (log(S/K) + (r + (pow(-1,j-1))*0.5*v*v)*T)/(v*(pow(T,0.5)));
-}
+// Pricing a European vanilla put option with a Monte Carlo method
+double monte_carlo_put_price(const int& num_sims, const double& S, const double& K, const double& r, const double& v, const double& T) {
+    double S_adjust = S * exp(T*(r-0.5*v*v));
+    double S_cur = 0.0;
+    double payoff_sum = 0.0;
 
-// Calculate the European vanilla call price based on
-// underlying S, strike K, risk-free rate r, volatility of
-// underlying sigma and time to maturity T
-double call_price(const double& S, const double& K, const double& r, const double& v, const double& T) {
-    return S * norm_cdf(d_j(1, S, K, r, v, T))-K*exp(-r*T) * norm_cdf(d_j(2, S, K, r, v, T));
-}
+    for (int i=0; i<num_sims; i++) {
+        double gauss_bm = gaussian_box_muller();
+        S_cur = S_adjust * exp(sqrt(v*v*T)*gauss_bm);
+        payoff_sum += std::max(K - S_cur, 0.0);
+    }
 
-// Calculate the European vanilla put price based on
-// underlying S, strike K, risk-free rate r, volatility of
-// underlying sigma and time to maturity T
-double put_price(const double& S, const double& K, const double& r, const double& v, const double& T) {
-    return -S*norm_cdf(-d_j(1, S, K, r, v, T))+K*exp(-r*T) * norm_cdf(-d_j(2, S, K, r, v, T));
+    return (payoff_sum / static_cast<double>(num_sims)) * exp(-r*T);
 }
 
 int main(int argc, char **argv) {
+    // First we create the parameter list
+    int num_sims = 10000000;   // Number of simulated asset paths
 
     // First we create the parameter list
     double S = 0;  // Option price
@@ -52,7 +66,10 @@ int main(int argc, char **argv) {
     double v = 0;    // Volatility of the underlying (20%)
     double T = 0;    // One year until expiry
 
+
     // Then we ask the user for the vaules of the parameters
+
+    std::cout << "Number of Paths: " << num_sims << std::endl;
     cout << "Option price?:    " <<  endl;
     cin >> S ;
     cout << "Strike price?:    " << endl;
@@ -65,12 +82,9 @@ int main(int argc, char **argv) {
     cin >> T ;
 
 
-
-
-
-    // Then we calculate the call/put values
-    double call = call_price(S, K, r, v, T);
-    double put = put_price(S, K, r, v, T);
+// Then we calculate the call/put values via Monte Carlo
+    double call = monte_carlo_call_price(num_sims, S, K, r, v, T);
+    double put = monte_carlo_put_price(num_sims, S, K, r, v, T);
 
     // Finally we output the parameters and prices
     cout << "Underlying:      " << S << endl;
